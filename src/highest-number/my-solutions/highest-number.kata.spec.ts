9db88303f5bfda79c4d719399da633addf7d11d8
Rex Zulfekar <rzulfekar@users.noreply.github.com>
2023-10-27 17:57:50 -0400

    complete highest number

---
+++ src/highest-number/my-solutions/highest-number.kata.spec.ts
@@ -0,0 +1,16 @@
+import { expect, describe, it } from 'vitest'
+import { getHighestNumber } from './highest-number.kata'
+
+describe('getHighestNumber', () => {
+  it('should get the highest number given an array of one number', () => {
+    const given = [42]
+    const actual = getHighestNumber(given)
+    expect(actual).toBe(42)
+  })
+
+  it('should get the highest number given an array of several numbers', () => {
+    const given = [1, 3, 2]
+    const actual = getHighestNumber(given)
+    expect(actual).toBe(3)
+  })
+})
